e573741e0b2b5c6f9ef85728dfb292f2f77699a3
Darsh09 <darsh0908@gmail.com>
2021-09-25 13:12:03 +0530

    Lstm added

--- app.py
+++ app.py
@@ -107,5 +107,82 @@ def ocr():
     else:
         return jsonify({ "result": "User does not exist" })
 
+@app.route('/api/naivebayes', methods=['POST'])
+def naivebayes():
+    id = request.json['id']
+    text = request.json['text']
+
+    r = requests.post(url = 'http://localhost:5000/api/user', json = { "id": id })
+    r=r.json()
+
+    if r["user"] == True:
+
+        # import re
+        # import nltk
+        # nltk.download('stopwords')
+        # from nltk.corpus import stopwords
+        # from nltk.stem.porter import PorterStemmer
+        # corpus = []
+        # review = re.sub('[^a-zA-Z]', ' ', text)
+        # review = review.lower()
+        # review = review.split()
+        # ps = PorterStemmer()
+        # review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+        # review = ' '.join(review)
+        # corpus.append(review)
+
+        # from sklearn.feature_extraction.text import CountVectorizer
+        # cv = CountVectorizer(max_features = 1500,ngram_range=(1,1))
+        # X = cv.fit_transform(corpus).toarray()
+        # # y = dataset.iloc[:, 1].values
+
+        # msg=corpus[0]
+        # ms0=cv.transform([msg])
+        # print(ms0)
+
+        # # from sklearn.externals import joblib
+        # import joblib
+        # nb = joblib.load('./models/nb.pkl')
+        # return jsonify({ "result": nb.predict(X) })
+
+        from tensorflow.keras.models import load_model
+        from tensorflow.keras.preprocessing.text import Tokenizer
+        from tensorflow.keras.preprocessing.sequence import pad_sequences
+        model = load_model('./lstm/best_model.h5')
+        import pickle
+        max_words = 5000
+        max_len=50
+        tokenizer = Tokenizer(num_words=max_words, lower=True, split=' ')
+        with open('./lstm/preprocess.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+        # tokenizer.fit_on_texts(text)
+        # tokenizer = Tokenizer(num_words=max_words, lower=True, split=' ')
+        def predict_class(text):
+            '''Function to predict sentiment class of the passed text'''
+            
+            sentiment_classes = ['Negative', 'Neutral', 'Positive']
+            max_len=50
+            # print(text)
+            # Transforms text to a sequence of integers using a tokenizer object
+            # tokenizer = Tokenizer()
+            
+            xt = tokenizer.texts_to_sequences(text)
+            print(xt)
+            # Pad sequences to the same length
+            xt = pad_sequences(xt, padding='post', maxlen=max_len)
+            print(xt)
+            # Do the prediction using the loaded model
+            yt = model.predict(xt).argmax(axis=1)
+            print(model.predict(xt))
+            # Print the predicted sentiment
+            k= sentiment_classes[yt[0]]
+            # print(k)
+            return k
+
+        return jsonify({ "result": predict_class([text])})
+
+    else:
+        return jsonify({ "result": "User does not exist" })
+
 if __name__ == '__main__':
     app.run(debug=True)
